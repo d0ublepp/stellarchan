729b205a4a82d3e9016bee5338ea76e321a96fa9
peawyoyoyin <piriyapong.l@kbtg.tech>
2018-06-20 16:20:42 +0700

    edit test

--- test/index.js
+++ test/index.js
@@ -17,16 +17,61 @@ import * as StellarSDK from 'stellar-sdk';
 const sinon = require("sinon");
 const chai = require("chai");
 const expect = chai.expect;
+const axios = require('axios')
+
+const mockLoadAccountResponse = require('./load-account-response.json')
+
 chai.use(require('chai-as-promised'))
 chai.should();
 //const StellarChanLib = require("../src/index.js");
-describe("getAccount", function() {
-  it('error 404', function(done) {
-      let stellarChan = new StellarChan(new StellarSDK.Server("https://horizon-testnet.stellar.org"));
-      let keypair = StellarSDK.Keypair.random();
-      expect(stellarChan.getAccount(keypair)).to.be.rejectedWith(/404/).notify(done)
+describe("StellarChan", function() {
+  beforeEach(function(done) {
+    this.axiosMock = sinon.mock(axios)
+    done();
   });
-});
+
+  afterEach(function(done) {
+    this.axiosMock.verify();
+    this.axiosMock.restore();
+    done();
+  });
+
+  describe("getAccount", function() {
+    it('test sinon mock!', function(done) {
+      this.axiosMock
+        .expects('get')
+        .withArgs(sinon.match('https://horizon-live.stellar.org:1337/accounts/GBAH7FQMC3CZJ4WD6GE7G7YXCIU36LC2IHXQ7D5MQAUO4PODOWIVLSFS'))
+        .returns(Promise.resolve({ data: mockLoadAccountResponse }))
+      
+      expect(axios.get('https://horizon-live.stellar.org:1337/accounts/GBAH7FQMC3CZJ4WD6GE7G7YXCIU36LC2IHXQ7D5MQAUO4PODOWIVLSFS'))
+        .to.eventually.deep.equal({ data: mockLoadAccountResponse }).notify(done)
+      // done();
+    });
+
+    it('runs smoothly!', async function(done) {
+      this.axiosMock
+        .expects('get')
+        .atLeast(1)
+        .withArgs(sinon.match('https://horizon-live.stellar.org:1337/accounts/GBAH7FQMC3CZJ4WD6GE7G7YXCIU36LC2IHXQ7D5MQAUO4PODOWIVLSFS'))
+        .returns(Promise.resolve({ data: mockLoadAccountResponse }))
+      
+      const mockKeypair = {
+        publicKey: () => 'GBAH7FQMC3CZJ4WD6GE7G7YXCIU36LC2IHXQ7D5MQAUO4PODOWIVLSFS'
+      }
+
+      const expected = await new StellarSDK.Server("https://horizon-live.stellar.org:1337").loadAccount('GBAH7FQMC3CZJ4WD6GE7G7YXCIU36LC2IHXQ7D5MQAUO4PODOWIVLSFS')
+      console.log(expected)
+      let stellarChan = new StellarChan(new StellarSDK.Server("https://horizon-live.stellar.org:1337"));
+      expect(stellarChan.getAccount(mockKeypair)).to.eventually.equal(expected).notify(done)
+    });
+
+    // it('error 404', function(done) {
+    //     let stellarChan = new StellarChan(new StellarSDK.Server("https://horizon-live.stellar.org:1337"));
+    //     let keypair = StellarSDK.Keypair.random();
+    //     expect(stellarChan.getAccount(keypair)).to.be.rejectedWith(/404/).notify(done);
+    // });
+  });
+})
 //https://horizon-testnet.stellar.org
 //getAccount(keypair)
 //createAccount(sourceKeypair, balance)
