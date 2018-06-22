f4fc917e1f4227c0db11a735557323cf5e677c22
jirawat <jirawat.p@cout-dev.com>
2018-06-22 10:42:57 +0700

    fix eslint

---
+++ test/.eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  'parserOptions': {
+    'ecmaVersion': 2017
+  },
+  'extends': 'airbnb',
+  'rules': {
+    'max-len': ['error', { 'code': 120 }]
+  },
+  "env": {
+    "mocha": true
+  }
+};
--- test/index.js
+++ test/index.js
@@ -1,71 +1,65 @@
-// import { assert } from 'chai';
-// import defaultAwesomeFunction, { awesomeFunction } from '../src';
-
-// describe('Awesome test.', () => {
-//   it('should test default awesome function', () => {
-//     const expectedVal = 'I am the Default Awesome Function, fellow comrade! - Dinesh'
-//     assert(defaultAwesomeFunction('Dinesh') === expectedVal, 'Default not awesome :(');
-//   });
-
-//   it('should test awesome function', () => {
-//     const expectedVal = 'I am just an Awesome Function'
-//     assert(awesomeFunction() === expectedVal, 'Named awesome :(');
-//   });
-// });
-import StellarChan from '../src/index.js';
+import { expect, should, use } from 'chai';
 import * as StellarSDK from 'stellar-sdk';
-const sinon = require("sinon");
-const chai = require("chai");
-const expect = chai.expect;
-const axios = require('axios')
+import * as sinon from 'sinon';
+import * as chaiAsPromised from 'chai-as-promised';
+import * as axios from 'axios';
+import StellarChan from '../src/index';
 
-const mockLoadAccountResponse = require('./load-account-response.json')
+use(chaiAsPromised);
+should();
 
-chai.use(require('chai-as-promised'))
-chai.should();
-//const StellarChanLib = require("../src/index.js");
+const mockLoadAccountResponse = require('./load-account-response.json');
+
+const stellarBaseUrl = 'https://horizon-live.stellar.org:1337';
+const testAccountAddress = 'GBAH7FQMC3CZJ4WD6GE7G7YXCIU36LC2IHXQ7D5MQAUO4PODOWIVLSFS';
+
+// const StellarChanLib = require("../src/index.js");
 
 const onlyNonFunctions = (obj) => {
-  const r = {}
-  Object.keys(obj).forEach(key => {
-    if(typeof obj[key] === 'object') {
-      r[key] = onlyNonFunctions(obj[key])
+  const r = {};
+  Object.keys(obj).forEach((key) => {
+    if (typeof obj[key] === 'object') {
+      r[key] = onlyNonFunctions(obj[key]);
     } else if (typeof obj[key] !== 'function') {
-      r[key] = obj[key]
+      r[key] = obj[key];
     }
-  })
-}
+  });
+};
 
-describe("StellarChan", function() {
-  beforeEach(function(done) {
-    this.axiosMock = sinon.mock(axios)
+describe('StellarChan', () => {
+  beforeEach((done) => {
+    this.axiosMock = sinon.mock(axios);
     done();
   });
 
-  afterEach(function(done) {
+  afterEach((done) => {
     this.axiosMock.verify();
     this.axiosMock.restore();
     done();
   });
 
-  describe("getAccount", function() {
-    it('returns correct account object!', async function() {
+  describe('getAccount', () => {
+    it('returns correct account object!', async () => {
       this.axiosMock
         .expects('get')
         .atLeast(1)
-        .withArgs(sinon.match('https://horizon-live.stellar.org:1337/accounts/GBAH7FQMC3CZJ4WD6GE7G7YXCIU36LC2IHXQ7D5MQAUO4PODOWIVLSFS'))
-        .returns(Promise.resolve({ data: mockLoadAccountResponse }))
-      
-      const mockKeypair = {
-        publicKey: () => 'GBAH7FQMC3CZJ4WD6GE7G7YXCIU36LC2IHXQ7D5MQAUO4PODOWIVLSFS'
-      }
+        .withArgs(sinon.match(`${stellarBaseUrl}/accounts/${testAccountAddress}`))
+        .returns(Promise.resolve({ data: mockLoadAccountResponse }));
 
-      const expected = await new StellarSDK.Server("https://horizon-live.stellar.org:1337").loadAccount('GBAH7FQMC3CZJ4WD6GE7G7YXCIU36LC2IHXQ7D5MQAUO4PODOWIVLSFS')
-      // console.log(expected)
-      let stellarChan = new StellarChan(new StellarSDK.Server("https://horizon-live.stellar.org:1337"));
-      let setllarAccount = await stellarChan.getAccount(mockKeypair)
+      const mockKeypair = {
+        publicKey: () => testAccountAddress,
+      };
+
+      const expected = await new StellarSDK.Server(stellarBaseUrl).loadAccount(testAccountAddress);
+
+      const stellarChan = new StellarChan(new StellarSDK.Server(stellarBaseUrl));
+      const setllarAccount = await stellarChan.getAccount(mockKeypair);
+
+      /* eslint-disable max-len */
       // expect(stellarChan.getAccount(mockKeypair).then(res => Promise.resolve(onlyNonFunctions(res))) ).to.eventually.equal(onlyNonFunctions(expected)).notify(done)
-      expect(onlyNonFunctions(setllarAccount)).to.deep.equal(onlyNonFunctions(expected))
+      /* eslint-enable max-len */
+
+      expect(onlyNonFunctions(setllarAccount)).to.deep.equal(onlyNonFunctions(expected));
     });
 
     // it('error 404', function(done) {
@@ -74,10 +68,12 @@ describe("StellarChan", function() {
     //     expect(stellarChan.getAccount(keypair)).to.be.rejectedWith(/404/).notify(done);
     // });
   });
-})
-//https://horizon-testnet.stellar.org
-//getAccount(keypair)
-//createAccount(sourceKeypair, balance)
-//trustAsset(trustKeypair, asset, limit)
-//createAssetPayment(sourceKeypair, destination, asset, assetAmount, timeboundsMin, timeboundsMax)
-//createChannel(sourceKeypair, destinationPublic, startingBalance, escrowAmount, asset, limit, timeboundsMin, timeboundsMax)
+});
+
+/* eslint-disable max-len */
+// https://horizon-testnet.stellar.org
+// getAccount(keypair)
+// createAccount(sourceKeypair, balance)
+// trustAsset(trustKeypair, asset, limit)
+// createAssetPayment(sourceKeypair, destination, asset, assetAmount, timeboundsMin, timeboundsMax)
+// createChannel(sourceKeypair, destinationPublic, startingBalance, escrowAmount, asset, limit, timeboundsMin, timeboundsMax)
