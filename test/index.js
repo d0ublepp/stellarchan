a78977780167c7300dab651d7ecbb13af41f1b5a
Chawin <ballpor98@gmail.com>
2018-06-25 14:13:46 +0700

    add createAccount test

--- test/index.js
+++ test/index.js
@@ -28,49 +28,68 @@ const onlyNonFunctions = (obj) => {
 };
 
 describe('StellarChan', () => {
-  beforeEach((done) => {
-    axiosMock = sinon.mock(axios);
-    done();
-  });
+      beforeEach((done) => {
+        axiosMock = sinon.mock(axios);
+        done();
+      });
 
-  afterEach((done) => {
-    axiosMock.verify();
-    axiosMock.restore();
-    done();
-  });
+      afterEach((done) => {
+        axiosMock.verify();
+        axiosMock.restore();
+        done();
+      });
 
-  describe('getAccount', () => {
-    it('returns correct account object!', async () => {
-      axiosMock
-        .expects('get')
-        .atLeast(1)
-        .withArgs(sinon.match(`${stellarBaseUrl}/accounts/${mockKeypair.publicKey()}`))
-        .returns(Promise.resolve({ data: mockLoadAccountResponse }));
+      describe('getAccount', () => {
+        it('returns correct account object!', async () => {
+          axiosMock
+            .expects('get')
+            .atLeast(1)
+            .withArgs(sinon.match(`${stellarBaseUrl}/accounts/${mockKeypair.publicKey()}`))
+            .returns(Promise.resolve({
+              data: mockLoadAccountResponse
+            }));
 
-      const expected = await new StellarSDK.Server(stellarBaseUrl).loadAccount(mockKeypair.publicKey());
+          const expected = await new StellarSDK.Server(stellarBaseUrl).loadAccount(mockKeypair.publicKey());
 
-      const stellarChan = new StellarChan(new StellarSDK.Server(stellarBaseUrl));
-      const setllarAccount = await stellarChan.getAccount(mockKeypair);
+          const stellarChan = new StellarChan(new StellarSDK.Server(stellarBaseUrl));
+          const setllarAccount = await stellarChan.getAccount(mockKeypair);
 
-      /* eslint-disable max-len */
-      // expect(stellarChan.getAccount(mockKeypair).then(res => Promise.resolve(onlyNonFunctions(res))) ).to.eventually.equal(onlyNonFunctions(expected)).notify(done)
-      /* eslint-enable max-len */
+          /* eslint-disable max-len */
+          // expect(stellarChan.getAccount(mockKeypair).then(res => Promise.resolve(onlyNonFunctions(res))) ).to.eventually.equal(onlyNonFunctions(expected)).notify(done)
+          /* eslint-enable max-len */
 
-      chai.expect(onlyNonFunctions(setllarAccount)).to.deep.equal(onlyNonFunctions(expected));
-    });
+          chai.expect(onlyNonFunctions(setllarAccount)).to.deep.equal(onlyNonFunctions(expected));
+        });
 
-    // it('error 404', function(done) {
-    //     let stellarChan = new StellarChan(new StellarSDK.Server("https://horizon-live.stellar.org:1337"));
-    //     let keypair = StellarSDK.Keypair.random();
-    //     expect(stellarChan.getAccount(keypair)).to.be.rejectedWith(/404/).notify(done);
-    // });
-  });
+        // it('error 404', function(done) {
+        //     let stellarChan = new StellarChan(new StellarSDK.Server("https://horizon-live.stellar.org:1337"));
+        //     let keypair = StellarSDK.Keypair.random();
+        //     expect(stellarChan.getAccount(keypair)).to.be.rejectedWith(/404/).notify(done);
+        // });
+      });
+      describe('createAccount', () => {
+        it('returns correct account object!', async () => {
+          axiosMock
+            .expects('get')
+            .atLeast(1)
+            .withArgs(sinon.match(`${stellarBaseUrl}/accounts/${mockKeypair.publicKey()}`))
+            .returns(Promise.resolve({
+              data: mockLoadAccountResponse
+            }));
+
+          axiosMock
+            .expects('post')
+            .atLeast(1)
+            .withArgs(sinon.match(`${stellarBaseUrl}/transactions`, sinon.match.string))
+            .returns(Promise.resolve({
+              data: {}
+            }));
+
+          const stellarChan = new StellarChan(new StellarSDK.Server(stellarBaseUrl));
+          StellarSDK.Config.setDefault();
+          StellarSDK.Network.useTestNetwork();
+          const result = stellarChan.createAccount(mockKeypair,"1");
+        });
+      });
 });
-
-/* eslint-disable max-len */
-// https://horizon-testnet.stellar.org
-// getAccount(keypair)
-// createAccount(sourceKeypair, balance)
-// trustAsset(trustKeypair, asset, limit)
-// createAssetPayment(sourceKeypair, destination, asset, assetAmount, timeboundsMin, timeboundsMax)
-// createChannel(sourceKeypair, destinationPublic, startingBalance, escrowAmount, asset, limit, timeboundsMin, timeboundsMax)
+      /* eslint-disable max-len */
